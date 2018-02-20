e37aabdd7e9cf384edf470d7ef314ed12016bb5a
XamaQueENois <gabrielsaito19@gmail.com>
2018-02-20 16:05:16 -0300

    Create valide.py

---
+++ valide.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+importe re
+var = raw_input("Digite seu e-mail: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	importe re
+var = raw_input("Digite seu nome: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	importe re
+var = raw_input("Digite seu telefone: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	importe re
+var = raw_input("Digite seu RG: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	importe re
+var = raw_input("Digite seu CPF: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	importe re
+var = raw_input("Digite sua data de nascimento: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	
+	importe re
+var = raw_input("Digite seu IP: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	importe re
+var = raw_input("Digite sua mascara de rede: ")
+if re.match(   ,var):
+	print("Valido")
+else:
+	print("Invalido")
+	
+	
+	
